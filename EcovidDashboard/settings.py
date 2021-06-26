91efe94931d7fa29abeb7d6c9ef08152d1f9f0c5
Mudit-Bagora <muditbagora@gmail.com>
2021-06-26 21:14:58 +0530

    deployment config

--- EcovidDashboard/settings.py
+++ EcovidDashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zvf%bvwh0c(&09f^-k&kqhyzdsu!@fu5apbaih&_abn6(4-u(c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecovid-dashboard.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
